a56617f9cad7664ea54969019ce2c83dbf6aabc3
dheanaura <oktavianidhea4@gmail.com>
2023-11-23 22:00:30 +0700

    added channel name

--- ecommerce/components/DetailProduct.vue
+++ ecommerce/components/DetailProduct.vue
@@ -9,13 +9,11 @@ const props = defineProps({
 <template>
 <section class="py-10">
 <div class="container">
-    <NuxtLink to="/product" class="bg-white border border-slate-300 w-max flex
-    items-center gap-1 py-2 px-5 rounded-full mb-7 cursor-pointer">
+    <NuxtLink to="/product" class="bg-white border border-slate-300 w-max flex items-center gap-1 py-2 px-5 rounded-full mb-7 cursor-pointer">
     <i class="ri-arrow-left-s-line text-base font-medium"></i>
     <span class="text-base font-medium">Kembali</span>
 </NuxtLink>
 <div class="flex items-center">
-
 <div class="w-1/2 bg-gray-300 mr-5 rounded-3xl flex justify-center items-
 center p-5 h-[500px]">
 
@@ -28,10 +26,7 @@ center p-5 h-[500px]">
 <p class="mb-10">{{ props.product.description }}</p>
 <div class="flex flex-col gap-4">
 
-<div class="w-full flex items-center gap-2 bg-blue-600 text-white py-
-3 justify-center rounded-lg cursor-pointer hover:bg-blue-600/80 transition
-
-duration-300 relative">
+<div class="w-full flex items-center gap-2 bg-blue-600 text-white py-3 justify-center rounded-lg cursor-pointer hover:bg-blue-600/80 transition duration-300 relative">
 <i class="ri-shopping-cart-2-line"></i>
 <span>Add to Cart</span>
 </div>
